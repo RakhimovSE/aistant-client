f0a9c5566b083cb574f24a6f5b5969b29e3e176d
RakhimovSE <JTzE8RYJeU9x8aw>
2023-09-17 01:37:41 +0400

    Create useUser hook

---
+++ app/hooks/use-user.ts
@@ -0,0 +1,14 @@
+import useSWR from "swr"
+
+import User from "@/app/types/user"
+import fetcher from "./fetcher"
+
+export default function useUser() {
+    const { data, error, isLoading } = useSWR<User>(`/api/user`, fetcher)
+
+    return {
+        user: data,
+        isLoading,
+        isError: error
+    }
+}
