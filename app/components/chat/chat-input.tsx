b472fa436941bf79d9d7d6cfc0e7a69a0d6447db
RakhimovSE <JTzE8RYJeU9x8aw>
2023-09-17 23:59:41 +0400

    Implement open ai completion in chat-input

--- app/components/chat/chat-input.tsx
+++ app/components/chat/chat-input.tsx
@@ -5,6 +5,7 @@ import {
   InputGroup,
   InputRightElement,
 } from '@chakra-ui/react'
+import { useCompletion } from 'ai/react'
 import { Field, FieldProps, Form, Formik } from 'formik'
 
 interface ChatInputFormValues {
@@ -12,32 +13,48 @@ interface ChatInputFormValues {
 }
 
 export default function ChatInput() {
+  const { completion, complete } = useCompletion()
+
   const initialValues: ChatInputFormValues = { message: '' }
 
   return (
-    <Formik initialValues={initialValues} onSubmit={console.log}>
-      <Form>
-        <Field name="message">
-          {({ field, form }: FieldProps<ChatInputFormValues['message']>) => (
-            <FormControl>
-              <InputGroup>
-                <Input pr="4.5rem" autoComplete="off" {...field} />
-                <InputRightElement width="4.5rem">
-                  <Button
-                    name="send"
-                    type="submit"
-                    h="1.75rem"
-                    size="sm"
-                    isLoading={form.isSubmitting}
-                  >
-                    Send
-                  </Button>
-                </InputRightElement>
-              </InputGroup>
-            </FormControl>
-          )}
-        </Field>
-      </Form>
-    </Formik>
+    <>
+      <div className="whitespace-pre-wrap my-6">{completion}</div>
+      <Formik
+        initialValues={initialValues}
+        onSubmit={async (values, actions) => {
+          await complete(values.message)
+          actions.setSubmitting(false)
+        }}
+      >
+        <Form>
+          <Field name="message">
+            {({ field, form }: FieldProps<ChatInputFormValues['message']>) => (
+              <FormControl>
+                <InputGroup>
+                  <Input
+                    pr="4.5rem"
+                    autoComplete="off"
+                    placeholder="Send a message"
+                    {...field}
+                  />
+                  <InputRightElement width="4.5rem">
+                    <Button
+                      name="send"
+                      type="submit"
+                      h="1.75rem"
+                      size="sm"
+                      isLoading={form.isSubmitting}
+                    >
+                      Send
+                    </Button>
+                  </InputRightElement>
+                </InputGroup>
+              </FormControl>
+            )}
+          </Field>
+        </Form>
+      </Formik>
+    </>
   )
 }
