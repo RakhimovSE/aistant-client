97cdbf65fd5ab3d26e060f52c482e902c95eb9aa
RakhimovSE <JTzE8RYJeU9x8aw>
2023-09-25 04:05:44 +0400

    next.config.js eslint

--- app/types/user.ts
+++
@@ -1,9 +0,0 @@
-type User = {
-  id: number
-  email: string
-  firstName: string
-  lastName: string
-  avatar: string
-}
-
-export default User
--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   env: {
-    title: 'AIstant',
+    title: "AIstant",
   },
 }
 
