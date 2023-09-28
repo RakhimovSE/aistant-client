74ba3ef9e351ae50cb6936ec76c8e2776e5d6b6c
RakhimovSE <JTzE8RYJeU9x8aw>
2023-09-28 22:50:34 +0400

    Create profile-menu.tsx component

--- app/components/header.tsx
+++ app/components/header.tsx
@@ -1,34 +1,21 @@
-import { FiBell, FiChevronDown, FiMenu } from "react-icons/fi"
+import { FiBell, FiMenu } from "react-icons/fi"
 import {
-  Avatar,
-  Box,
   Flex,
   FlexProps,
   HStack,
   IconButton,
-  Menu,
-  MenuButton,
-  MenuDivider,
-  MenuItem,
-  MenuList,
-  Skeleton,
-  SkeletonCircle,
   Spacer,
   Text,
   UseDisclosureReturn,
-  VStack,
   useColorModeValue,
 } from "@chakra-ui/react"
-import { useUserQuery } from "@/gql/graphql"
+import ProfileMenu from "./profile-menu"
 
 interface NavbarProps extends FlexProps {
   onSidebarOpen: UseDisclosureReturn["onOpen"]
 }
 
 function Navbar({ onSidebarOpen, ...rest }: NavbarProps) {
-  const { data } = useUserQuery({ variables: { userId: "1" } })
-  const user = data?.user
-
   return (
     <Flex
       p="1rem"
@@ -53,65 +40,14 @@ function Navbar({ onSidebarOpen, ...rest }: NavbarProps) {
 
       <Spacer display={{ base: "none", md: "block" }} />
 
-      <HStack spacing={{ base: "0", md: "6" }}>
+      <HStack spacing="1rem">
         <IconButton
           size="lg"
           variant="ghost"
           aria-label="open menu"
           icon={<FiBell />}
         />
-        <Flex alignItems="center">
-          <Menu>
-            <MenuButton
-              py={2}
-              transition="all 0.3s"
-              _focus={{ boxShadow: "none" }}
-            >
-              <HStack>
-                {user ? (
-                  <Avatar size="sm" src={user.avatar || undefined} />
-                ) : (
-                  <SkeletonCircle />
-                )}
-                <VStack
-                  display={{ base: "none", md: "flex" }}
-                  alignItems="flex-start"
-                  spacing="1px"
-                  ml="2"
-                >
-                  {user ? (
-                    <>
-                      <Text fontSize="sm">
-                        {user.firstName} {user.lastName}
-                      </Text>
-                      <Text fontSize="xs" color="gray.600">
-                        {user.email}
-                      </Text>
-                    </>
-                  ) : (
-                    <>
-                      <Skeleton fontSize="sm">Sample user</Skeleton>
-                      <Skeleton fontSize="xs">sample@email.com</Skeleton>
-                    </>
-                  )}
-                </VStack>
-                <Box display={{ base: "none", md: "flex" }}>
-                  <FiChevronDown />
-                </Box>
-              </HStack>
-            </MenuButton>
-            <MenuList
-              bg={useColorModeValue("white", "gray.900")}
-              borderColor={useColorModeValue("gray.200", "gray.700")}
-            >
-              <MenuItem>Profile</MenuItem>
-              <MenuItem>Settings</MenuItem>
-              <MenuItem>Billing</MenuItem>
-              <MenuDivider />
-              <MenuItem>Sign out</MenuItem>
-            </MenuList>
-          </Menu>
-        </Flex>
+        <ProfileMenu />
       </HStack>
     </Flex>
   )
---
+++ app/components/profile-menu.tsx
@@ -0,0 +1,82 @@
+import { ReactElement } from "react"
+import { FiChevronDown } from "react-icons/fi"
+import {
+  Avatar,
+  Box,
+  HStack,
+  Menu,
+  MenuButton,
+  MenuDivider,
+  MenuItem,
+  MenuList,
+  Skeleton,
+  SkeletonCircle,
+  Text,
+  VStack,
+  useColorModeValue,
+} from "@chakra-ui/react"
+import { useUserQuery } from "@/gql/graphql"
+
+type ButtonComponents = {
+  avatarComponent: ReactElement
+  nameComponent: ReactElement
+  emailComponent: ReactElement
+}
+
+export default function ProfileMenu() {
+  const { data } = useUserQuery({ variables: { userId: "1" } })
+  const user = data?.user
+
+  const { avatarComponent, nameComponent, emailComponent }: ButtonComponents =
+    user
+      ? {
+          avatarComponent: <Avatar size="sm" src={user.avatar || undefined} />,
+          nameComponent: (
+            <Text fontSize="sm">
+              {user.firstName} {user.lastName}
+            </Text>
+          ),
+          emailComponent: (
+            <Text fontSize="xs" color="gray.600">
+              {user.email}
+            </Text>
+          ),
+        }
+      : {
+          avatarComponent: <SkeletonCircle />,
+          nameComponent: <Skeleton fontSize="sm">Sample user</Skeleton>,
+          emailComponent: <Skeleton fontSize="xs">sample@email.com</Skeleton>,
+        }
+
+  return (
+    <Menu>
+      <MenuButton py={2} transition="all 0.3s" _focus={{ boxShadow: "none" }}>
+        <HStack>
+          {avatarComponent}
+          <VStack
+            display={{ base: "none", md: "flex" }}
+            alignItems="flex-start"
+            spacing="1px"
+            ml="2"
+          >
+            {nameComponent}
+            {emailComponent}
+          </VStack>
+          <Box display={{ base: "none", md: "flex" }}>
+            <FiChevronDown />
+          </Box>
+        </HStack>
+      </MenuButton>
+      <MenuList
+        bg={useColorModeValue("white", "gray.900")}
+        borderColor={useColorModeValue("gray.200", "gray.700")}
+      >
+        <MenuItem>Profile</MenuItem>
+        <MenuItem>Settings</MenuItem>
+        <MenuItem>Billing</MenuItem>
+        <MenuDivider />
+        <MenuItem>Sign out</MenuItem>
+      </MenuList>
+    </Menu>
+  )
+}
