32304d358a9b0625b95a117d92266272acb36a12
RakhimovSE <JTzE8RYJeU9x8aw>
2023-10-24 01:21:13 +0400

    Refactor layout.tsx, providers.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,38 @@
 import { ReactNode } from "react"
 import { Metadata } from "next"
 import Script from "next/script"
-import ApolloProvider from "./apollo-provider"
 import Providers from "./providers"
 
 export const metadata: Metadata = {
   title: process.env.title,
 }
+
+const YANDEX_METRIKA_SCRIPT_HTML = `
+(function(m, e, t, r, i, k, a) {
+  m[i] = m[i] || function() {
+      (m[i].a = m[i].a || []).push(arguments)
+  };
+  m[i].l = 1 * new Date();
+  for (var j = 0; j < document.scripts.length; j++) {
+      if (document.scripts[j].src === r) {
+          return;
+      }
+  }
+  k = e.createElement(t), a = e.getElementsByTagName(t)[0], k.async = 1, k.src = r, a.parentNode.insertBefore(k, a)
+})(window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+ym(95036405, "init", {
+  clickmap: true,
+  trackLinks: true,
+  accurateTrackBounce: true,
+  webvisor: true
+});
+`
+
+const YANDEX_METRIKA_NOSCRIPT_HTML = `
+<div>
+  <img src="https://mc.yandex.ru/watch/95036405" style="position:absolute; left:-9999px;" alt="" />
+</div>
+`
 
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
@@ -15,21 +41,15 @@ export default function RootLayout({ children }: { children: ReactNode }) {
         <Script
           id="yandex-metrika"
           strategy="afterInteractive"
-          dangerouslySetInnerHTML={{
-            __html: `(function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)}; m[i].l=1*new Date(); for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }} k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)}) (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym"); ym(95036405, "init", { clickmap:true, trackLinks:true, accurateTrackBounce:true, webvisor:true });`,
-          }}
+          dangerouslySetInnerHTML={{ __html: YANDEX_METRIKA_SCRIPT_HTML }}
         />
         <noscript
           // eslint-disable-next-line react/no-danger
-          dangerouslySetInnerHTML={{
-            __html: `<div><img src="https://mc.yandex.ru/watch/95036405" style="position:absolute; left:-9999px;" alt="" /></div>`,
-          }}
+          dangerouslySetInnerHTML={{ __html: YANDEX_METRIKA_NOSCRIPT_HTML }}
         />
       </head>
       <body>
-        <Providers>
-          <ApolloProvider>{children}</ApolloProvider>
-        </Providers>
+        <Providers>{children}</Providers>
       </body>
     </html>
   )
--- app/providers.tsx
+++ app/providers.tsx
@@ -3,11 +3,14 @@
 import { ReactNode } from "react"
 import { CacheProvider } from "@chakra-ui/next-js"
 import { ChakraProvider } from "@chakra-ui/react"
+import ApolloProvider from "./apollo-provider"
 
 export default function Providers({ children }: { children: ReactNode }) {
   return (
     <CacheProvider>
-      <ChakraProvider>{children}</ChakraProvider>
+      <ApolloProvider>
+        <ChakraProvider>{children}</ChakraProvider>
+      </ApolloProvider>
     </CacheProvider>
   )
 }
