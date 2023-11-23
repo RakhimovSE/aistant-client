20433b053e5081013d4db5e3e2200d11598691be
RakhimovSE <JTzE8RYJeU9x8aw>
2023-11-23 17:37:42 +0400

    Add VkProvider

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,14 +1,19 @@
 import NextAuth from "next-auth"
-import GoogleProviders from "next-auth/providers/google"
-import YandexProviders from "next-auth/providers/yandex"
+import GoogleProvider from "next-auth/providers/google"
+import VkProvider from "next-auth/providers/vk"
+import YandexProvider from "next-auth/providers/yandex"
 
 const handler = NextAuth({
   providers: [
-    GoogleProviders({
+    GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID as string,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
     }),
-    YandexProviders({
+    VkProvider({
+      clientId: process.env.VK_CLIENT_ID as string,
+      clientSecret: process.env.VK_CLIENT_SECRET as string,
+    }),
+    YandexProvider({
       clientId: process.env.YANDEX_CLIENT_ID as string,
       clientSecret: process.env.YANDEX_CLIENT_SECRET as string,
     }),
