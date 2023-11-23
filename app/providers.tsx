81a1e0bee642ed7e35d7b9f5f608b0acec8300cb
RakhimovSE <JTzE8RYJeU9x8aw>
2023-11-23 17:04:26 +0400

    Remove graphql

--- app/providers.tsx
+++ app/providers.tsx
@@ -3,14 +3,11 @@
 import { ReactNode } from "react"
 import { CacheProvider } from "@chakra-ui/next-js"
 import { ChakraProvider } from "@chakra-ui/react"
-import ApolloProvider from "./apollo-provider"
 
 export default function Providers({ children }: { children: ReactNode }) {
   return (
     <CacheProvider>
-      <ApolloProvider>
-        <ChakraProvider>{children}</ChakraProvider>
-      </ApolloProvider>
+      <ChakraProvider>{children}</ChakraProvider>
     </CacheProvider>
   )
 }
